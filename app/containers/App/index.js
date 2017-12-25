715855e3593b2c78da9bfd48aa74b6b6055cfb3c
MohammadAmin Vahedinia <github@vahedinia.me>
2017-12-26 00:15:40 +0330

    fixed fucking footer issue :|

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="siteBody">
         <Helmet
           defaultTitle="BeePaste"
         >
